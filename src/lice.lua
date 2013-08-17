da9068634fbf004202790b294141a01ae29e72c3
Yonaba <roland.yonaba@gmail.com>
2013-08-17 19:42:18 +0000

    Fixed check for dependency

--- src/lice.lua
+++ src/lice.lua
@@ -14,7 +14,7 @@ local ROOT_PATH = arg[0]:match('(.+)[//\].+$')
 -- Check for dependencies
 local lfs
 do
-  assert(pcall(require, 'a'), 'Dependency LuaFileSystem not found.\nCannot execute Lice-Lua')
+  assert(pcall(require, 'lfs'), 'Dependency LuaFileSystem not found.\nCannot execute Lice-Lua')
   lfs = require 'lfs'
 end
 
